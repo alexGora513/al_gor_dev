9b96d55348c96839ed224c3d0493c7012c5dbc8c
DESKTOP-6B16U0O\админ <oleks.hora@gmail.com>
2017-01-29 05:29:10 +0200

    New changes and product

---
+++ src/main/NewGitHub.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by админ on 29.01.2017.
+ */
+public class NewGitHub {
+
+    public static void main(String[]args){
+        System.out.println("New project");
+    }
+}
